5997bd1e2eaa522dcc958d418dd7ee1cc33df430
Kaiwei Ang <akw522@live.com>
2016-11-13 21:15:37 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     name='shi7en',
     version=__version__,
     packages=find_packages(),
-    package_data = {'ninja_shi7': ['*.fa']},
+    package_data = {'ninja_shi7': ['adapters/*.fa']},
     #include_package_data=True,
     url='',
     license=__license__,
