754a7b7447d4bd71a26f735fcb9322bea77bb8a9
Kaiwei Ang <lancekaiwei@gmail.com>
2016-12-26 22:34:02 -0600

    testing command line script again

--- setup.py
+++ setup.py
@@ -13,16 +13,17 @@ __version__ = "0.0.1-dev"
 
 long_description = 'All of your shi7 is one place. From quality scores to mappable reads.'
 
+if platform.system() == 'Darwin':
+    print('Darwin found!')
+else:
+    print('Darwin not found!')
+
 setup(
     name='shi7en',
     version=__version__,
     packages=find_packages(),
     package_data = {'shi7en': ['adapters/*.fa']},
     url='',
-    if platform.system() == 'Darwin':
-        print('Darwin found!')
-    else:
-        print('Darwin not found!')
     license=__license__,
     author=__author__,
     author_email=__email__,
