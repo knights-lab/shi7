6c335f9f9ad413eaf9206b9f0b2e8137e2a3fe34
Kaiwei Ang <lancekaiwei@gmail.com>
2016-12-27 01:10:31 -0600

    updating setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ __version__ = "0.0.1-dev"
 long_description = 'All of your shi7 is one place. From quality scores to mappable reads.'
 
 if platform.system() == 'Darwin':
-    bin_path = 'bin/shi7en_trim_mac'.encode(encoding='UTF-8')
+    bin_path = 'bin/shi7en_trim_mac'    #.encode(encoding='UTF-8')
 else:
-    bin_path = 'bin/shi7en_trim_linux'.encode(encoding='UTF-8')
+    bin_path = 'bin/shi7en_trim_linux'  #.encode(encoding='UTF-8')
 
 setup(
     name='shi7en',
@@ -31,10 +31,10 @@ setup(
     long_description=long_description,
     keywords='',
     install_requires=[],
+    #scripts=[bin_path],
     entry_points={
         'console_scripts': [
             'shi7en = shi7en.shi7en:main',
         ]
     },
-    scripts=[bin_path.decode('UTF-8')],
 )
