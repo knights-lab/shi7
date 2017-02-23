42ac9ed4e9833e848fef9ca4c6f6089f6501d4e9
Benjamin Hillmann <bhillmann@outlook.com>
2017-02-23 15:37:38 -0600

    Switched to UnitTest

--- tests/test_fastq_gen.py
+++ tests/test_fastq_gen.py
@@ -1,19 +1,21 @@
 from shi7.learning import read_fastq
 import os
+import unittest
+
+from nose.tools import assert_equals
 
 
-def test_fastq_gen():
-    path_fastqs = [os.path.join('testfq', f) for f in os.listdir('testfq') if f.endswith('fastq')]
-    i = 0
-    j = 0
-    for path_fastq in path_fastqs:
-        with open(path_fastq) as inf:
-            fastq_gen = read_fastq(inf)
-            for title, data, qualities in fastq_gen:
-                i += 1
-        with open(path_fastq) as inf:
-            for line in inf:
-                j += 1
-    assert i == j/4
-
-test_fastq_gen()
+class TestFastqGen(unittest.TestCase):
+    def test(self):
+        path_fastqs = [os.path.join('testfq', f) for f in os.listdir('testfq') if f.endswith('fastq')]
+        i = 0
+        j = 0
+        for path_fastq in path_fastqs:
+            with open(path_fastq) as inf:
+                fastq_gen = read_fastq(inf)
+                for title, data, qualities in fastq_gen:
+                    i += 1
+            with open(path_fastq) as inf:
+                for line in inf:
+                    j += 1
+        assert i == j/4
