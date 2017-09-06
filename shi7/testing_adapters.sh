b909b467d705bcbe6908cfc942354e29a92bd2d2
Benjamin Hillmann <bhillmann@outlook.com>
2017-09-06 17:01:33 -0500

    Testing adapters.

--- shi7/testing_adapters.sh
+++ shi7/testing_adapters.sh
@@ -1,4 +1,5 @@
-shi7_learning.py -i ./testfq/hmpTS2 -o ./test
-shi7_learning.py -i ./testfq/mctNEX -o ./test
-shi7_learning.py -i ./testfq/mkyTS3 -o ./test
-shi7_learning.py -i ./testfq/pmpNEX -o ./test
+./shi7_learning.py -t 1 -i ./testfq/hmpTS2 -o ./test/hmpTS2 &
+./shi7_learning.py -t 1 -i ./testfq/mctNEX -o ./test/mctNEX &
+./shi7_learning.py -t 1 -i ./testfq/mkyTS3 -o ./test/mkyTS3 &
+./shi7_learning.py -t 1 -i ./testfq/pmpNEX -o ./test/pmpNEX &
+./shi7_learning.py -t 1 -i ./testfq/GA01NEX -o ./test/GAO1NEX &
