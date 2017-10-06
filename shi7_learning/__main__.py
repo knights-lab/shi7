4c80eedc091da6677dda6ddd610ce57bcc30a174
Benjamin Hillmann <bhillmann@outlook.com>
2017-10-06 13:38:53 -0500

    Added scripts to make the zipfile.

---
+++ shi7_learning/__main__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env
+import shi7
+shi7.shi7_learning.main()
--- zip_app.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python -m zipapp shi7 -m 'shi7_learner:main' -o shi7_learner.pyz
-python -m zipapp shi7 -m 'shi7:main' -o shi7.pyz
