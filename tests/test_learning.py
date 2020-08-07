abdf7ce944af742196f2beaf5fca1d1a0b3b3f82
wkg <wkg@williamgasper.com>
2020-08-07 15:21:54 -0500

    testing ci

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -15,7 +15,7 @@ def test_subsample():
     os.makedirs(output_path)
     avg_sequence_len, qual_scores = get_seq_length_qual_scores(path_fastqs, output_path, num_sequences=200, num_files=3)
     shutil.rmtree(output_path)
-    assert(None, None)
+    #assert(None, None)
 
 
 def test_axe_adaptors(subsampled_fastqs):
