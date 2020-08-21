a2afea735053c9babf5f4f65e860322c1decdfa9
William Gasper <wkg@williamgasper.com>
2020-08-21 07:23:28 -0500

    adapting to nosetests framework

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -3,8 +3,9 @@ from shi7.shi7 import *
 import os
 import shutil
 
+from nose.tools import assert_equals
 
-def test_subsample():
+def subsample():
     # Fetch example fastqs
     path_fastqs = [os.path.join('testfq', f) for f in os.listdir('testfq') if f.endswith('fastq')]
     # Create a fake output directory
@@ -15,10 +16,11 @@ def test_subsample():
     os.makedirs(output_path)
     avg_sequence_len, qual_scores = get_seq_length_qual_scores(path_fastqs, output_path, num_sequences=200, num_files=3)
     shutil.rmtree(output_path)
-    assert(None, None)
+    #assert_equals(None, None)
 
 
-def test_axe_adaptors(subsampled_fastqs):
+def axe_adaptors(subsampled_fastqs):
+    #subsampled_fastqs = os.path.join("testfq", "temp", "subsampled_fastqs")
     path_fastqs = [os.path.join('testfq', f) for f in os.listdir('testfq') if f.endswith('fastq')]
     output_path = os.path.join("testfq", "temp", "axe_adaptors")
     if os.path.exists(output_path):
@@ -33,7 +35,7 @@ def test_axe_adaptors(subsampled_fastqs):
     return best_adap
 
 
-def test_flash(adapter_output_filenames):
+def flash(adapter_output_filenames):
     print('adapter_output_filenames:', adapter_output_filenames)
     return_vars = []
     flash_output_path = os.path.join("testfq", "temp", "flash")
@@ -51,7 +53,7 @@ def test_flash(adapter_output_filenames):
 
     return sum(return_vars,[])
 
-def test_trimmer(flash_output_filenames):
+def trimmer(flash_output_filenames):
     print('flash_output_filenames:', flash_output_filenames)
     trimmer_output_path = os.path.join("testfq", "temp", "trimmer")
     if os.path.exists(trimmer_output_path):
@@ -60,16 +62,15 @@ def test_trimmer(flash_output_filenames):
     filter_q, trim_q = trimmer_learning(flash_output_filenames)
     print(filter_q, trim_q)
 
-if __name__ == '__main__':
-
-    test_subsample()
+def test_learning_routine():
+    subsample()
     subsampled_fastqs = os.path.join("testfq", "temp", "subsampled_fastqs")
 
     if os.path.exists(subsampled_fastqs):
         shutil.rmtree(subsampled_fastqs)
 
     os.makedirs(subsampled_fastqs)
-    best_adap = test_axe_adaptors(subsampled_fastqs) # This will also generate the subsampled fastqs
+    best_adap = axe_adaptors(subsampled_fastqs) # This will also generate the subsampled fastqs
     path_subsampled_fastqs = [os.path.join(subsampled_fastqs, f) for f in os.listdir(subsampled_fastqs) if f.endswith('fastq')]
     adapter_output_path = os.path.join("testfq", "temp", "axe_adaptors")
 
@@ -83,9 +84,10 @@ if __name__ == '__main__':
         adapter_output_filenames = axe_adaptors_paired_end(path_subsampled_fastqs, adapter_output_path, best_adap, threads, shell=False)
     else:
         adapter_output_filenames = path_subsampled_fastqs
-
+    
+    assert_equals(None, None)
 #    if detect_paired_end(path_subsampled_fastqs):
-#        flash_vars = test_flash(adapter_output_filenames)
+#        flash_vars = flash(adapter_output_filenames)
 #        print(flash_vars)
 #        if flash_vars[0]:   #if it is stitchable
 #            flash_output_path = os.path.join("testfq", "temp", "flash")
@@ -101,4 +103,7 @@ if __name__ == '__main__':
 #            flash_output_filenames = adapter_output_filenames   #skip FLASH
 #
 #
-#    test_trimmer(flash_output_filenames)
+#    trimmer(flash_output_filenames)
+
+if __name__ == '__main__':
+    test_learning_routine()
