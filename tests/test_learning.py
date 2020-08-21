0ac654c885b5a60aab638f5fede8d92fee3b14d9
William Gasper <wkg@williamgasper.com>
2020-08-21 07:31:33 -0500

    added needed args to choose_axe_adaptors call

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -20,7 +20,9 @@ def subsample():
 
 
 def axe_adaptors(subsampled_fastqs):
-    #subsampled_fastqs = os.path.join("testfq", "temp", "subsampled_fastqs")
+    num_threads = 1
+    paired_end = True
+
     path_fastqs = [os.path.join('testfq', f) for f in os.listdir('testfq') if f.endswith('fastq')]
     output_path = os.path.join("testfq", "temp", "axe_adaptors")
     if os.path.exists(output_path):
@@ -28,7 +30,7 @@ def axe_adaptors(subsampled_fastqs):
     os.makedirs(output_path)
     get_seq_length_qual_scores(path_fastqs, subsampled_fastqs, num_sequences=200) #To create the subsampled_fastqs
     path_subsampled_fastqs = [os.path.join(subsampled_fastqs, f) for f in os.listdir(subsampled_fastqs) if f.endswith('fastq')]
-    best_adap, best_size = choose_axe_adaptors(path_subsampled_fastqs, output_path)
+    best_adap, best_size = choose_axe_adaptors(path_subsampled_fastqs, paired_end, output_path, num_threads)
     print("Best adaptor:", best_adap)
     print("Best adaptor size:", best_size)
 
@@ -62,48 +64,47 @@ def trimmer(flash_output_filenames):
     filter_q, trim_q = trimmer_learning(flash_output_filenames)
     print(filter_q, trim_q)
 
-def test_learning_routine():
-    subsample()
-    subsampled_fastqs = os.path.join("testfq", "temp", "subsampled_fastqs")
+class TestLearning(unittest.TestCase):
+    def test():
+        subsample()
+        subsampled_fastqs = os.path.join("testfq", "temp", "subsampled_fastqs")
 
-    if os.path.exists(subsampled_fastqs):
-        shutil.rmtree(subsampled_fastqs)
+        if os.path.exists(subsampled_fastqs):
+            shutil.rmtree(subsampled_fastqs)
 
-    os.makedirs(subsampled_fastqs)
-    best_adap = axe_adaptors(subsampled_fastqs) # This will also generate the subsampled fastqs
-    path_subsampled_fastqs = [os.path.join(subsampled_fastqs, f) for f in os.listdir(subsampled_fastqs) if f.endswith('fastq')]
-    adapter_output_path = os.path.join("testfq", "temp", "axe_adaptors")
+        os.makedirs(subsampled_fastqs)
+        best_adap = axe_adaptors(subsampled_fastqs) # This will also generate the subsampled fastqs
+        path_subsampled_fastqs = [os.path.join(subsampled_fastqs, f) for f in os.listdir(subsampled_fastqs) if f.endswith('fastq')]
+        adapter_output_path = os.path.join("testfq", "temp", "axe_adaptors")
 
-    if os.path.exists(adapter_output_path):
-        shutil.rmtree(adapter_output_path)
+        if os.path.exists(adapter_output_path):
+            shutil.rmtree(adapter_output_path)
 
-    os.makedirs(adapter_output_path)
+        os.makedirs(adapter_output_path)
 
-    if best_adap:
-        threads = min(multiprocessing.cpu_count(),16)
-        adapter_output_filenames = axe_adaptors_paired_end(path_subsampled_fastqs, adapter_output_path, best_adap, threads, shell=False)
-    else:
-        adapter_output_filenames = path_subsampled_fastqs
-    
-    assert_equals(None, None)
-#    if detect_paired_end(path_subsampled_fastqs):
-#        flash_vars = flash(adapter_output_filenames)
-#        print(flash_vars)
-#        if flash_vars[0]:   #if it is stitchable
-#            flash_output_path = os.path.join("testfq", "temp", "flash")
-#            if os.path.exists(flash_output_path):
-#                shutil.rmtree(flash_output_path)
-#                os.makedirs(flash_output_path)
-#            if flash_vars[2] <= 0.1:
-#                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=int(round(flash_vars[3])), min_overlap=int(round(flash_vars[4])), allow_outies=flash_vars[1], threads=threads, shell=False)
-#            else:
-#                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=700, min_overlap=20, allow_outies=flash_vars[2], threads=threads, shell=False)
-#            flash_output_filenames = flash_part2(flash_output_str, flash_output_path)
-#        else:
-#            flash_output_filenames = adapter_output_filenames   #skip FLASH
-#
-#
-#    trimmer(flash_output_filenames)
+        if best_adap:
+            threads = min(multiprocessing.cpu_count(),16)
+            adapter_output_filenames = axe_adaptors_paired_end(path_subsampled_fastqs, adapter_output_path, best_adap, threads, shell=False)
+        else:
+            adapter_output_filenames = path_subsampled_fastqs
+        
+        assert_equals(None, None)
+    #    if detect_paired_end(path_subsampled_fastqs):
+    #        flash_vars = flash(adapter_output_filenames)
+    #        print(flash_vars)
+    #        if flash_vars[0]:   #if it is stitchable
+    #            flash_output_path = os.path.join("testfq", "temp", "flash")
+    #            if os.path.exists(flash_output_path):
+    #                shutil.rmtree(flash_output_path)
+    #                os.makedirs(flash_output_path)
+    #            if flash_vars[2] <= 0.1:
+    #                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=int(round(flash_vars[3])), min_overlap=int(round(flash_vars[4])), allow_outies=flash_vars[1], threads=threads, shell=False)
+    #            else:
+    #                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=700, min_overlap=20, allow_outies=flash_vars[2], threads=threads, shell=False)
+    #            flash_output_filenames = flash_part2(flash_output_str, flash_output_path)
+    #        else:
+    #            flash_output_filenames = adapter_output_filenames   #skip FLASH
+    #
+    #
+    #    trimmer(flash_output_filenames)
 
-if __name__ == '__main__':
-    test_learning_routine()
