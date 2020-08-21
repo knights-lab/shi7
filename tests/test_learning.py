cd7b5c9a8ada40d15c01ca063fe58ae436c864bb
William Gasper <wkg@williamgasper.com>
2020-08-21 09:30:55 -0500

    uncommented flash related lines

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -18,7 +18,6 @@ def subsample():
     os.makedirs(output_path)
     avg_sequence_len, qual_scores = get_seq_length_qual_scores(path_fastqs, output_path, num_sequences=200, num_files=3)
     shutil.rmtree(output_path)
-    #assert_equals(None, None)
 
 
 def axe_adaptors(subsampled_fastqs):
@@ -91,22 +90,23 @@ class TestLearning(unittest.TestCase):
             adapter_output_filenames = path_subsampled_fastqs
         
         assert_equals(None, None)
-    #    if detect_paired_end(path_subsampled_fastqs):
-    #        flash_vars = flash(adapter_output_filenames)
-    #        print(flash_vars)
-    #        if flash_vars[0]:   #if it is stitchable
-    #            flash_output_path = os.path.join("testfq", "temp", "flash")
-    #            if os.path.exists(flash_output_path):
-    #                shutil.rmtree(flash_output_path)
-    #                os.makedirs(flash_output_path)
-    #            if flash_vars[2] <= 0.1:
-    #                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=int(round(flash_vars[3])), min_overlap=int(round(flash_vars[4])), allow_outies=flash_vars[1], threads=threads, shell=False)
-    #            else:
-    #                flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=700, min_overlap=20, allow_outies=flash_vars[2], threads=threads, shell=False)
-    #            flash_output_filenames = flash_part2(flash_output_str, flash_output_path)
-    #        else:
-    #            flash_output_filenames = adapter_output_filenames   #skip FLASH
-    #
-    #
-    #    trimmer(flash_output_filenames)
-
+        if detect_paired_end(path_subsampled_fastqs):
+            flash_vars = flash(adapter_output_filenames)
+            print(flash_vars)
+            if flash_vars[0]:   #if it is stitchable
+                flash_output_path = os.path.join("testfq", "temp", "flash")
+                if os.path.exists(flash_output_path):
+                    shutil.rmtree(flash_output_path)
+                    os.makedirs(flash_output_path)
+                if flash_vars[2] <= 0.1:
+                    flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=int(round(flash_vars[3])), min_overlap=int(round(flash_vars[4])), allow_outies=flash_vars[1], threads=threads, shell=False)
+                else:
+                    flash_output_str = flash_part1(adapter_output_filenames, flash_output_path, max_overlap=700, min_overlap=20, allow_outies=flash_vars[2], threads=threads, shell=False)
+                flash_output_filenames = flash_part2(flash_output_str, flash_output_path)
+            else:
+                flash_output_filenames = adapter_output_filenames   #skip FLASH
+    
+    
+        trimmer(flash_output_filenames)
+        
+        assert_equals(None, None)
