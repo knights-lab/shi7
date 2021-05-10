0f15dd53684ecf542800aec622f2c66a6be29671
bhillmann <hillmannben@gmail.com>
2021-05-10 12:14:41 -0500

    stop iteration fixed in limit_fastq and subset fastq

--- shi7/shi7_learning.py
+++ shi7/shi7_learning.py
@@ -28,11 +28,12 @@ def make_arg_parser():
     parser.set_defaults()
     return parser
 
+
 def subsample_fastqs(path_fastqs, num_files=10, num_sequences=1000):
     for i, path_fastq in enumerate(path_fastqs):
+        if i >= num_files:
+            return
         with open(path_fastq) as fastq_inf:
-            if i >= num_files:
-                break
             fastq_gen = read_fastq(fastq_inf)
             yield limit_fastq(fastq_gen, num_sequences=num_sequences)
 
